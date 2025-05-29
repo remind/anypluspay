4f50abbefa4c157b60b2eb42dfb7e37121971ff1
wxj <408934054@qq.com>
2025-05-29 18:17:55 +0800

    增加参数验证

--- app/admin/app/application/src/main/java/com/anypluspay/admin/auth/request/AuthLoginRequest.java
+++ app/admin/app/application/src/main/java/com/anypluspay/admin/auth/request/AuthLoginRequest.java
@@ -1,6 +1,6 @@
 package com.anypluspay.admin.auth.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -15,14 +15,14 @@ public class AuthLoginRequest {
     /**
      * 用户名
      */
-    @NotNull(message = "用户名不能为空")
+    @NotBlank(message = "用户名不能为空")
     @Length(min = 5, max = 18, message = "用户名长度为5-18")
     private String username;
 
     /**
      * 密码
      */
-    @NotNull(message = "密码不能为空")
+    @NotBlank(message = "密码不能为空")
     @Length(min = 6, max = 18, message = "密码长度为6-18")
     private String password;
 }
