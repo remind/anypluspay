ff35750b803c83a746feb6c3bd8932f1f07b7582
wxj <408934054@qq.com>
2025-06-04 14:55:38 +0800

    处理支付宝异步通知

--- app/channel-gateway/alipay-gateway/src/main/java/com/anypluspay/channelgateway/alipay/AlipayVerifySignGateway.java
+++ app/channel-gateway/alipay-gateway/src/main/java/com/anypluspay/channelgateway/alipay/AlipayVerifySignGateway.java
@@ -1,8 +1,9 @@
 package com.anypluspay.channelgateway.alipay;
 
-import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.alipay.api.AlipayConstants;
 import com.alipay.api.domain.AlipayTradeQueryModel;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradeQueryRequest;
 import com.alipay.api.response.AlipayTradeQueryResponse;
 import com.anypluspay.channelgateway.api.verify.VerifyModel;
@@ -14,6 +15,8 @@ import com.anypluspay.commons.enums.ResultStatusEnum;
 import com.anypluspay.commons.lang.types.Money;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * 通知验签
@@ -27,12 +30,12 @@ public class AlipayVerifySignGateway extends AbstractAlipayGateway implements Ve
     @Override
     public void notify(GatewayRequest<VerifyModel> request, VerifySignResult result) {
         VerifyModel verifyModel = request.getContent();
-        JSONObject jsonObject = JSONUtil.parseObj(verifyModel.getRequestBody());
+        Map<String, String> paramsMap = JSONUtil.toBean(verifyModel.getRequestBody(), Map.class);
         if (CallbackType.PAGE.getCode().equals(verifyModel.getCallbackType())) {
             // 构造请求参数以调用接口
             AlipayTradeQueryRequest alipayRequest = new AlipayTradeQueryRequest();
             AlipayTradeQueryModel model = new AlipayTradeQueryModel();
-            model.setOutTradeNo(jsonObject.getStr("out_trade_no"));
+            model.setOutTradeNo(paramsMap.get("out_trade_no"));
             alipayRequest.setBizModel(model);
             try {
                 AlipayTradeQueryResponse response = build().execute(alipayRequest);
@@ -40,6 +43,25 @@ public class AlipayVerifySignGateway extends AbstractAlipayGateway implements Ve
                 result.setApiCode(response.getTradeStatus());
                 result.setRealAmount(new Money(response.getTotalAmount()));
                 result.setSuccess(true);
+            } catch (Exception e) {
+                log.error("支付宝验签异常", e);
+                result.setSuccess(false);
+                result.setApiCode(ResultStatusEnum.UNKNOWN.getCode());
+                result.setApiMessage(e.getMessage());
+            }
+        } else if (CallbackType.SERVER.getCode().equals(verifyModel.getCallbackType())) {
+            try {
+                boolean signVerified = AlipaySignature.rsaCheckV1(paramsMap, getPublicKey(), AlipayConstants.CHARSET, AlipayConstants.SIGN_TYPE); //调用SDK验证签名
+                if (signVerified) {
+                    result.setInstRequestNo(paramsMap.get("out_trade_no"));
+                    result.setApiCode(paramsMap.get("trade_status"));
+                    result.setRealAmount(new Money(paramsMap.get("total_amount")));
+                    result.setSuccess(true);
+                } else {
+                    result.setSuccess(false);
+                    result.setApiCode(ResultStatusEnum.UNKNOWN.getCode());
+                    result.setApiMessage("验签不通过");
+                }
             } catch (Exception e) {
                 log.error("支付宝验签异常", e);
                 result.setSuccess(false);
