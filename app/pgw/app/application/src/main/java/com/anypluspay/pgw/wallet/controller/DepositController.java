5365e3468cd519cc675f2e9365f9617371d11da5
wxj <408934054@qq.com>
2025-07-02 23:09:05 +0800

    钱包充值接口

--- app/pgw/app/application/src/main/java/com/anypluspay/pgw/wallet/controller/DepositController.java
+++ app/pgw/app/application/src/main/java/com/anypluspay/pgw/wallet/controller/DepositController.java
@@ -1,12 +1,20 @@
 package com.anypluspay.pgw.wallet.controller;
 
 import com.anypluspay.commons.response.ResponseResult;
+import com.anypluspay.pgw.cashier.model.CashierPayResult;
+import com.anypluspay.pgw.cashier.model.CashierType;
+import com.anypluspay.pgw.cashier.request.DepositPayRequest;
+import com.anypluspay.pgw.cashier.response.WebPayMethodResponse;
+import com.anypluspay.pgw.cashier.service.CashierService;
 import com.anypluspay.pgw.wallet.AbstractWalletController;
-import com.anypluspay.pgw.wallet.response.deposit.DepositResponse;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
+ * 充值
+ *
  * @author wxj
  * 2025/7/1
  */
@@ -14,4 +22,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/wallet/deposit")
 public class DepositController extends AbstractWalletController {
 
+    @Autowired
+    private CashierService cashierService;
+
+    /**
+     * 获取充值的支付方式
+     *
+     * @return
+     */
+    @GetMapping("/pay-methods")
+    public ResponseResult<List<WebPayMethodResponse>> payMethods() {
+        return ResponseResult.success(cashierService.buildPayMethods(CashierType.DEPOSIT, getLoginMember().getMemberId()));
+    }
+
+    /**
+     * 提交充值
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/submit")
+    @ResponseBody
+    public ResponseResult<CashierPayResult> submit(@RequestBody DepositPayRequest request) {
+        request.setPartnerId("100000003");
+        request.setMemberId(getLoginMember().getMemberId());
+        return ResponseResult.success(cashierService.depositPay(request));
+    }
 }
