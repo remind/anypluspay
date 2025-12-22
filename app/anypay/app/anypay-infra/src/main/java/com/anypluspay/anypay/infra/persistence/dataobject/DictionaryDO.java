831f64a7630b76cd46b0e6ac28b5d08a47ebcc67
wxj <408934054@qq.com>
2025-12-22 21:15:38 +0800

    新增dalgen

---
+++ app/anypay/app/anypay-infra/src/main/java/com/anypluspay/anypay/infra/persistence/dataobject/DictionaryDO.java
@@ -0,0 +1,131 @@
+package com.anypluspay.anypay.infra.persistence.dataobject;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 字典表
+ * </p>
+ *
+ * @author wxj
+ * @since 2025-12-22
+ */
+@TableName("tc_dictionary")
+public class DictionaryDO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 字典类型
+     */
+    @TableId(value = "type", type = IdType.NONE)
+    private String type;
+
+    /**
+     * 编码
+     */
+    @TableId(value = "code", type = IdType.NONE)
+    private String code;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 备注
+     */
+    private String memo;
+
+    /**
+     * 扩展信息
+     */
+    private String extra;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime gmtCreate;
+
+    /**
+     * 最后修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime gmtModified;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    public LocalDateTime getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(LocalDateTime gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public LocalDateTime getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(LocalDateTime gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    @Override
+    public String toString() {
+        return "DictionaryDO{" +
+        "type = " + type +
+        ", code = " + code +
+        ", name = " + name +
+        ", memo = " + memo +
+        ", extra = " + extra +
+        ", gmtCreate = " + gmtCreate +
+        ", gmtModified = " + gmtModified +
+        "}";
+    }
+}
---
+++ app/anypay/app/anypay-infra/src/main/java/com/anypluspay/anypay/infra/persistence/mapper/DictionaryMapper.java
@@ -0,0 +1,16 @@
+package com.anypluspay.anypay.infra.persistence.mapper;
+
+import com.anypluspay.anypay.infra.persistence.dataobject.DictionaryDO;
+import com.anypluspay.component.dal.mybatis.ext.ExtBaseMapper;
+
+/**
+ * <p>
+ * 字典表 Mapper 接口
+ * </p>
+ *
+ * @author wxj
+ * @since 2025-12-22
+ */
+public interface DictionaryMapper extends ExtBaseMapper<DictionaryDO> {
+
+}
